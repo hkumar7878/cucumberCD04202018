ceaf226d50f5133ca9cd2a579311f7c933e6d0a2
hkumar7878 <hitesh.ghai@hotmail.com>
2018-04-13 20:11:20 +0530

    Commit on 04/13

--- cucumberframework/src/test/java/com/cucumber/framework/runner/WomenProductVerification/WomenProductVerification.java
+++ cucumberframework/src/test/java/com/cucumber/framework/runner/WomenProductVerification/WomenProductVerification.java
@@ -1,19 +1,43 @@
 package com.cucumber.framework.runner.WomenProductVerification;
 
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import cucumber.api.testng.TestNGCucumberRunner;
 
 
 @CucumberOptions(features={"classpath:featurefile/productCategory/TestTotalNumberOfWomenDressProduct.feature"}
 ,glue={"classpath:com.cucumber.framework.stepdefinition",
 	   "classpath:com.cucumber.framework.Helper"},
 		plugin = {"pretty", "html:target/cucumber-html-report"},
-		dryRun=true,
+		dryRun=false,
 		monochrome = false
 )
 
-public class WomenProductVerification extends AbstractTestNGCucumberTests{
+public class WomenProductVerification {
+	
+	@BeforeSuite
+	
+	public void beforeSuite()
+	{
+		System.out.println("Before running the suite");
+	}
+	
+	@AfterSuite
+	public void afterSuite()
+	{
+		System.out.println("After running the suite");
+	}
+	
+	@Test()
+	public void runCukes()
+	{
+		new TestNGCucumberRunner(getClass()).runCukes();
+	}
 
 }
 
